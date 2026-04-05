52d0d6e12e7b1b328bdbdf42be6b70a625235c1f
Matheus Santos <162858255+TheuzinCode@users.noreply.github.com>
2026-04-05 00:07:20 -0300

    Criação do projeto e conexão com banco de dados

---
+++ WillBaber/src/main/java/projetoBarbearia/WillBaber/WillBaberApplication.java
@@ -0,0 +1,13 @@
+package projetoBarbearia.WillBaber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillBaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillBaberApplication.class, args);
+	}
+
+}
---
+++ WillBaber/src/test/java/projetoBarbearia/WillBaber/WillBaberApplicationTests.java
@@ -0,0 +1,13 @@
+package projetoBarbearia.WillBaber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillBaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
