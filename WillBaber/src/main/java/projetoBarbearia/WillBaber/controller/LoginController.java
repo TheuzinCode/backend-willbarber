7d3c8b2af5513d75bf1c6115510a09f05e6c72ae
Matheus Santos <matheus.gssantos01@gmail.com>
2026-04-24 00:19:45 -0300

    autorizando rotas

--- WillBaber/src/main/java/projetoBarbearia/WillBaber/controller/LoginController.java
+++ WillBaber/src/main/java/projetoBarbearia/WillBaber/controller/LoginController.java
@@ -2,16 +2,15 @@ package projetoBarbearia.WillBaber.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import projetoBarbearia.WillBaber.domain.Login.dto.LoginRequestDTO;
 import projetoBarbearia.WillBaber.domain.Login.dto.LoginResponseDTO;
 import projetoBarbearia.WillBaber.service.LoginService;
 
 import java.util.Optional;
 
+
+@CrossOrigin("*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/willbarber")
